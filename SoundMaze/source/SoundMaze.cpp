160658771f52f1d624a122bfcab08dd32d56eb41
Claudio-R <claudio98.rc@gmail.com>
2022-12-06 11:11:35 +0100

    Refactoring

--- SoundMaze/source/SoundMaze.cpp
+++ SoundMaze/source/SoundMaze.cpp
@@ -1,19 +1,42 @@
 /* TODO:
  * ok. Correctly upload the map
- * ok. Fix the map
- * ok. Correctly implement collisions
- * 4. Textures
- * 5. Fix shaders
- * 6. Add lights
- * 7. Add sound
+ * #ok Fix the map
+ * #ok Correctly implement collisions
+ * #ok Fix shaders
+ * # Add treasures
+ * # Improve textures
+ * # Fix map to have it easyer to work with
+ * # Add sound
  */
 
 #include "SoundMaze.hpp"
 
 const std::string MODEL_PATH_MAZE = "models/maze_translated.obj";
 const std::string TEXTURE_PATH_MAZE = "textures/maze_rock.jpg";
-const std::string MODEL_PATH_TREASURE = "models/icosphere.obj";
-const std::string TEXTURE_PATH_TREASURE = "textures/maze_rock.jpg";
+
+constexpr float ROT_SPEED = glm::radians(60.0f);
+const float MOVE_SPEED = 0.5f;
+const float MOUSE_RES = 500.0f;
+constexpr float FOVY = glm::radians(60.0f);
+const float NEAR_FIELD = 0.001f;
+const float FAR_FIELD = 5.0f;
+constexpr float MAX_UP_ANGLE = glm::radians(70.0f);
+constexpr float MAX_DOWN_ANGLE = glm::radians(-70.0f);
+
+struct Object {
+	alignas(16) Model model;
+	alignas(16) Texture texture;
+	alignas(16) glm::vec4 specular_color;
+};
+
+struct Pipe {
+	alignas(16) Pipeline pipeline;
+	alignas(16) DescriptorSetLayout dsl_global;
+	alignas(16) DescriptorSet ds_global;
+	alignas(16) DescriptorSetLayout dsl;
+	alignas(16) DescriptorSet ds;
+	alignas(16) int setsInPipe = 2;
+};
 
 // UNIFORMS
 struct ModelViewProjection {
@@ -23,41 +46,28 @@ struct ModelViewProjection {
 };
 
 struct Material {
-	alignas(16) glm::vec3 specular;
-	alignas(16) float shininess;
+	alignas(16) glm::vec4 specular_color;
 };
-
-struct Light {
-	alignas(16) glm::mat4 model;
-	alignas(16) glm::vec3 position;
-	alignas(16) glm::vec3 ambient;
-	alignas(16) glm::vec3 diffuse;
-	alignas(16) glm::vec3 specular;
-};
+// UNIFORMS
  
 class SoundMaze : public BaseProject {
 protected:
-	// Map
+
 	int map_width, map_height;
+	Object maze{};
+	Pipe p_graphic;
+
 	stbi_uc* map;
 
-	Pipeline P_maze; // MAZE
-	//Pipeline P_treasure; // TREASURES
+	//Pipeline P_maze;
+	//DescriptorSetLayout DSL_mvp;
+	//DescriptorSetLayout DSL_maze;
+	//DescriptorSet DS_mvp;
 
-	DescriptorSetLayout DSL_mvp; // equal for both pipelines
-	DescriptorSetLayout DSL_maze;
-	//DescriptorSetLayout DSL_treasure;
+	//Model M_maze;
+	//Texture T_maze;
+	//DescriptorSet DS_maze;
 
-	DescriptorSet DS_mvp;
-
-	Model M_maze;
-	Texture T_maze;
-	DescriptorSet DS_maze;
-
-	//Model M_treasure;
-	//Texture T_treasure;
-	//DescriptorSet DS_treasure;
-	
 	void setWindowParameters() {	
 		windowWidth = 1200;
 		windowHeight = 800;
@@ -66,146 +76,103 @@ protected:
 		
 		uniformBlocksInPool = 3;
 		texturesInPool = 1;
-		setsInPool = 2;
+		//setsInPool = 2;
+		setsInPool = p_graphic.setsInPipe;
 	}
 	
-	void loadMap() {
-		map = stbi_load("textures/high-constrast-maze-map-specular.png", &map_width, &map_height, NULL, 1);
-		if (map) {
-			std::cout << "Map loaded! Size --> " << map_width << "x" << map_height << std::endl;
-			/* Si muove dando precedenza alle righe */
-		}
-	}
-
 	void localInit() {
+		map = stbi_load("textures/high-constrast-maze-map-specular.png", &map_width, &map_height, NULL, 1);
+		maze.model.init(this, MODEL_PATH_MAZE);
+		maze.texture.init(this, TEXTURE_PATH_MAZE);
+		maze.specular_color = glm::vec4(1.0, 1.0, 1.0, 32);
 
-		loadMap();
-		std::cout << remap(glm::vec2(0, 0)).x << std::endl;
-		std::cout << remap(glm::vec2(0, 5)).x << std::endl;
-		std::cout << remap(glm::vec2(5, 0)).x << std::endl;
-		std::cout << remap(glm::vec2(5, 5)).x << std::endl;
+		//DSL_mvp.init(this, { {0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT} });
+		p_graphic.dsl_global.init(this, { {0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT} });
 
-		/* DESCRIPTOR SET LAYOUTS */
-		DSL_mvp.init(this, {
-			{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT},
-			});
-
-		DSL_maze.init(this, {
+		//DSL_maze.init(this, {
+		//maze.dsl.init(this, {
+		p_graphic.dsl.init(this, {
 			{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT},
-			{1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT},
-			{2, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT}
+			{1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT}
 			});
 
-		//DSL_treasure.init(this, {
-		//	{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_FRAGMENT_BIT},
-		//	{1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT}
-		//	});
+		//P_maze.init(this, "shaders/vert0.spv", "shaders/frag0.spv", { &DSL_mvp, &DSL_maze });
+		p_graphic.pipeline.init(this, "shaders/vert0.spv", "shaders/frag0.spv", { &p_graphic.dsl_global, &p_graphic.dsl });
 
-		/* PIPELINE */
-		P_maze.init(this, "shaders/vert0.spv", "shaders/frag0.spv", { &DSL_mvp, &DSL_maze });
-		//P_treasure.init(this, "shaders/vert1.spv", "shaders/frag1.spv", { &DSL_mvp, &DSL_treasure });
 
-		/* DESCRIPTOR SETS */
-		DS_mvp.init(this, &DSL_mvp, {
-			{0, UNIFORM, sizeof(ModelViewProjection), nullptr}
-		});
-		
-		/* MAZE */
-		M_maze.init(this, MODEL_PATH_MAZE);
-		T_maze.init(this, TEXTURE_PATH_MAZE);
-		DS_maze.init(this, &DSL_maze, {
+		//DS_mvp.init(this, &DSL_mvp, { {0, UNIFORM, sizeof(ModelViewProjection), nullptr} });
+		p_graphic.ds_global.init(this, &p_graphic.dsl_global, { {0, UNIFORM, sizeof(ModelViewProjection), nullptr} });
+		//M_maze.init(this, MODEL_PATH_MAZE);
+		//T_maze.init();
+		//DS_maze.init(this, &DSL_maze, {
+		//maze.ds.init(this, &maze.dsl, {
+		p_graphic.ds.init(this, &p_graphic.dsl, {
 			{0, UNIFORM, sizeof(Material), nullptr},
-			{1, UNIFORM, sizeof(Light), nullptr},
-			{2, TEXTURE, 0, &T_maze},
+			{1, TEXTURE, 0, &maze.texture},
 		});
-		
-		/* TREASURES */
-		//M_treasure.init(this, MODEL_PATH_TREASURE);
-		//T_treasure.init(this, TEXTURE_PATH_TREASURE);
-		//DS_treasure.init(this, &DSL_treasure, {
-		//	{0, UNIFORM, sizeof(Light), nullptr},
-		//	{1, TEXTURE, 0, &T_treasure}
-		//});
 	}
 		
 	void populateCommandBuffer(VkCommandBuffer commandBuffer, int currentImage) {
-		
-		/* PIPELINE 1 */
-		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, P_maze.graphicsPipeline);
-		
-		// Set0 - MVP
-		vkCmdBindDescriptorSets(commandBuffer, 
-			VK_PIPELINE_BIND_POINT_GRAPHICS, 
-			P_maze.pipelineLayout, 
-			0, 1, &DS_mvp.descriptorSets[currentImage],
-			0, nullptr);
-		
-		// Set1 - Maze
-		VkBuffer vertexBuffers_maze[] = { M_maze.vertexBuffer };
+		//VkBuffer vertexBuffers_maze[] = { M_maze.vertexBuffer };
+		VkBuffer vertexBuffers_maze[] = { maze.model.vertexBuffer };
 		VkDeviceSize offsets_maze[] = { 0 };
-		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers_maze, offsets_maze);
-		vkCmdBindIndexBuffer(commandBuffer, M_maze.indexBuffer, 0, VK_INDEX_TYPE_UINT32);
-		vkCmdBindDescriptorSets(commandBuffer, 
-			VK_PIPELINE_BIND_POINT_GRAPHICS, 
-			P_maze.pipelineLayout, 1, 1, &DS_maze.descriptorSets[currentImage], 
+
+		//vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, P_maze.graphicsPipeline);
+		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, p_graphic.pipeline.graphicsPipeline);
+		
+		vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, 
+			//P_maze.pipelineLayout, 0, 1, &DS_mvp.descriptorSets[currentImage],
+			p_graphic.pipeline.pipelineLayout, 0, 1, &p_graphic.ds_global.descriptorSets[currentImage],
 			0, nullptr);
+
+		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers_maze, offsets_maze);
+		//vkCmdBindIndexBuffer(commandBuffer, M_maze.indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		vkCmdBindIndexBuffer(commandBuffer, maze.model.indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+		vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, 
+			//P_maze.pipelineLayout, 1, 1, &DS_maze.descriptorSets[currentImage], 
+			p_graphic.pipeline.pipelineLayout, 1, 1, &p_graphic.ds.descriptorSets[currentImage],
+			0, nullptr);
+
 		vkCmdDrawIndexed(commandBuffer, 
-			static_cast<uint32_t>(M_maze.indices.size()), 1, 0, 0, 0);
-		
-		/* PIPELINE 2 */
-		//vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, P_treasure.graphicsPipeline);
-		//vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, P_treasure.pipelineLayout, 0, 1, &DS_mvp.descriptorSets[currentImage], 0, nullptr);
-		//VkBuffer vertexBuffers_treasures[] = { M_treasure.vertexBuffer };
-		//VkDeviceSize offsets_treasure[] = { 0 };
-		//vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers_treasures, offsets_treasure);
-		//vkCmdBindIndexBuffer(commandBuffer, M_treasure.indexBuffer, 0, VK_INDEX_TYPE_UINT32);
-		//vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, P_treasure.pipelineLayout, 1, 1, &DS_treasure.descriptorSets[currentImage], 0, nullptr);
-		//vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(M_treasure.indices.size()), 1, 0, 0, 0);
-
-	}
-
-	glm::vec2 remap(glm::vec2 v) {
-
-		int pixX = map_width - round(fmax(0.0f, fmin(map_width - 1, (-v.x / 10.0f) * map_width)));
-		int pixY = map_height - round(fmax(0.0f, fmin(map_height - 1, (-v.y / 10.0f) * map_height)));
-
-		double model_radius = 5; //5 cm
-		float x = v.x;
-		float y = v.y;
-		
-		x = glm::clamp(double(x), -model_radius, model_radius) + model_radius; // [0, 10]
-		x = x / (2 * model_radius); // [0, 1]
-		int x_p = round(x * map_width); // [0, 1017]
-
-		y = glm::clamp(double(y), -model_radius, model_radius) + model_radius; // [0, 5.2]
-		y = y / (2 * model_radius); // [0, 1]
-		int y_p = round(y * map_width);
-		return glm::vec2(pixX, pixY);
-	}
-
-	bool canStepPoint(float x, float y) {
-		glm::vec2 v = remap(glm::vec2(x, y));
-		return map[map_width * int(v.x) + int(v.y)] != 0;
+			//static_cast<uint32_t>(M_maze.indices.size()), 1, 0, 0, 0);
+			static_cast<uint32_t>(maze.model.indices.size()), 1, 0, 0, 0);
 	}
 
 	bool canStep(float x, float y) {
-		//std::cout << x << " " << y << std::endl;
-		glm::vec2 v = remap(glm::vec2(x, y));
-		//std::cout << "Pixels: " << v.x << " " << v.y << std::endl;
 
-		static float checkRadius = 0.1;
-		static int checkSteps = 12;
+		static const float checkRadius = 0.15;
+		static const int checkSteps = 4;
+		static const float model_diameter = 10.0f;
+
 		for (int i = 0; i < checkSteps; i++) {
 			double check_x = x + cos(6.2832 * i / (float)checkSteps) * checkRadius;
 			double check_y = y + sin(6.2832 * i / (float)checkSteps) * checkRadius;
-			if(!canStepPoint(check_x, check_y)) return false;
+
+			int x_p = map_width - round(fmax(0.0f, fmin(map_width - 1, (- check_x / model_diameter) * map_width)));
+			int y_p = map_height - round(fmax(0.0f, fmin(map_height - 1, (- check_y / model_diameter) * map_height)));
+			
+			bool walkable = map[map_width * int(x_p) + int(y_p)] != 0;
+			if(!walkable) return false;
 		}
+
 		return true;
 	}
 
 	void updateCameraAngles(glm::vec3 *CamAng, float deltaT, float ROT_SPEED) {
-		static constexpr float MAX_UP_ANGLE = glm::radians(70.0f);
-		static constexpr float MAX_DOWN_ANGLE = glm::radians(-70.0f);
+		static double old_xpos = 0, old_ypos = 0;
+		double xpos, ypos;
+
+		glfwGetCursorPos(window, &xpos, &ypos);
+		double m_dx = xpos - old_xpos;
+		double m_dy = ypos - old_ypos;
+		old_xpos = xpos; old_ypos = ypos;
+
+		glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GLFW_TRUE);
+
+		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+			CamAng->y += m_dx * ROT_SPEED / MOUSE_RES;
+			CamAng->x += m_dy * ROT_SPEED / MOUSE_RES;
+		}
 
 		if (glfwGetKey(window, GLFW_KEY_LEFT)) {
 			CamAng->y += deltaT * 2 * ROT_SPEED;
@@ -227,8 +194,10 @@ protected:
 	}
 
 	void updateCameraPosition(glm::vec3* CamPos, float deltaT, float MOVE_SPEED, glm::vec3 CamAng) {
+		
 		glm::vec3 oldCamPos = *CamPos;
 		glm::vec3 newCamPos = *CamPos;
+		
 		if (glfwGetKey(window, GLFW_KEY_A)) {
 			newCamPos -= MOVE_SPEED * glm::vec3(glm::rotate(glm::mat4(1.0f), CamAng.y,
 				glm::vec3(0.0f, 1.0f, 0.0f)) * glm::vec4(1, 0, 0, 1)) * deltaT;
@@ -253,128 +222,88 @@ protected:
 		*CamPos = newCamPos;
 	}
 
-	float computeMVP(ModelViewProjection* mvp) {
+	ModelViewProjection computeMVP(glm::vec3 camAng, glm::vec3 camPos) {
+		
+		ModelViewProjection mvp{};
+		glm::mat3 CamDir = glm::mat3(glm::rotate(glm::mat4(1.0f), camAng.y, glm::vec3(0.0f, 1.0f, 0.0f))) *
+			glm::mat3(glm::rotate(glm::mat4(1.0f), camAng.x, glm::vec3(1.0f, 0.0f, 0.0f))) *
+			glm::mat3(glm::rotate(glm::mat4(1.0f), camAng.z, glm::vec3(0.0f, 0.0f, 1.0f)));
+
+		glm::mat4 CamMat = glm::translate(glm::transpose(glm::mat4(CamDir)), -camPos);
+
+		glm::mat4 model = glm::mat4(1.0);
+		mvp.model = glm::translate(
+			glm::rotate(
+				glm::translate(
+					model, 
+					glm::vec3(-5, 0, -5)
+				), 
+				glm::radians(90.0f), 
+				glm::vec3(0, 1, 0)
+			),
+			glm::vec3(5, 0, 5)
+		);
+
+		mvp.view = CamMat;
+		mvp.proj = glm::perspective(FOVY, swapChainExtent.width / (float)swapChainExtent.height, NEAR_FIELD, FAR_FIELD);
+		mvp.proj[1][1] *= -1;
+
+		return mvp;
+
+	}
+
+	void updateUniformBuffer(uint32_t currentImage) {
 
 		static auto startTime = std::chrono::high_resolution_clock::now();
 		static float lastTime = 0.0f;
-
 		auto currentTime = std::chrono::high_resolution_clock::now();
 		float time = std::chrono::duration<float, std::chrono::seconds::period>
 			(currentTime - startTime).count();
 		float deltaT = time - lastTime;
 		lastTime = time;
 
-		static constexpr float ROT_SPEED = glm::radians(60.0f);
-		static const float MOVE_SPEED = 0.5f;
-		static const float MOUSE_RES = 500.0f;
-
-		static glm::vec3 CamAng = glm::vec3(0.0f);
-		static glm::vec3 CamPos = glm::vec3(-5.0f, .1f, -5.0f);
-		static double old_xpos = 0, old_ypos = 0;
-		double xpos, ypos;
-
-		glfwGetCursorPos(window, &xpos, &ypos);
-		double m_dx = xpos - old_xpos;
-		double m_dy = ypos - old_ypos;
-		old_xpos = xpos; old_ypos = ypos;
-
-		glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GLFW_TRUE);
-
-		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
-			CamAng.y += m_dx * ROT_SPEED / MOUSE_RES;
-			CamAng.x += m_dy * ROT_SPEED / MOUSE_RES;
-		}
-
-		updateCameraAngles(&CamAng, deltaT, ROT_SPEED);
-
-		glm::mat3 CamDir = glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.y, glm::vec3(0.0f, 1.0f, 0.0f))) *
-			glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.x, glm::vec3(1.0f, 0.0f, 0.0f))) *
-			glm::mat3(glm::rotate(glm::mat4(1.0f), CamAng.z, glm::vec3(0.0f, 0.0f, 1.0f)));
-
-		updateCameraPosition(&CamPos, deltaT, MOVE_SPEED, CamAng);
-
-		glm::mat4 CamMat = glm::translate(glm::transpose(glm::mat4(CamDir)), -CamPos);
-
-		constexpr float FOVY = glm::radians(60.0f);
-		const float NEAR_FIELD = 0.001f;
-		const float FAR_FIELD = 5.0f;
-
-		glm::mat4 model = glm::rotate(glm::scale(glm::mat4(1.0f), glm::vec3(-1, 1, -1)), (float)glm::radians(-35.0f), glm::vec3(0, 1, 0));
-		mvp->model = glm::translate(
-			glm::rotate(
-				glm::translate(glm::mat4(1.0), glm::vec3(-5, 0, -5)), glm::radians(90.0f), glm::vec3(0, 1, 0)),
-			glm::vec3(5, 0, 5));
-			
-		mvp->view = CamMat;
-		mvp->proj = glm::perspective(FOVY, swapChainExtent.width / (float)swapChainExtent.height, NEAR_FIELD, FAR_FIELD);
-		mvp->proj[1][1] *= -1;
-
-		return time;
-	}
-
-	void updateUniformBuffer(uint32_t currentImage) {
-
 		void* data;
-
-		ModelViewProjection mvp{};
-		float time = computeMVP(&mvp);
-		vkMapMemory(device, DS_mvp.uniformBuffersMemory[0][currentImage], 0, sizeof(mvp), 0, &data);
+		static glm::vec3 camAng = glm::vec3(0.0f);
+		static glm::vec3 camPos = glm::vec3(-5.0f, .1f, -5.0f);
+		updateCameraAngles(&camAng, deltaT, ROT_SPEED);
+		updateCameraPosition(&camPos, deltaT, MOVE_SPEED, camAng);
+		ModelViewProjection mvp = computeMVP(camAng, camPos);
+		//vkMapMemory(device, DS_mvp.uniformBuffersMemory[0][currentImage], 0, sizeof(mvp), 0, &data);
+		vkMapMemory(device, p_graphic.ds_global.uniformBuffersMemory[0][currentImage], 0, sizeof(mvp), 0, &data);
 		memcpy(data, &mvp, sizeof(mvp));
-		vkUnmapMemory(device, DS_mvp.uniformBuffersMemory[0][currentImage]);
+		//vkUnmapMemory(device, DS_mvp.uniformBuffersMemory[0][currentImage]);
+		vkUnmapMemory(device, p_graphic.ds_global.uniformBuffersMemory[0][currentImage]);
 
-		Material maze{};
-		maze.specular = glm::vec3(0.5f, 0.5f, 0.5f);
-		maze.shininess = 32.0f;
-		vkMapMemory(device, DS_maze.uniformBuffersMemory[0][currentImage], 0, sizeof(maze), 0, &data);
-		memcpy(data, &maze, sizeof(maze));
-		vkUnmapMemory(device, DS_maze.uniformBuffersMemory[0][currentImage]);
 
-		Light light{};
-	/*	light.model = glm::translate(glm::mat4(1.0f),
-			glm::vec3(light.position)) * glm::rotate(glm::mat4(1.0f), time * glm::radians(70.0f + 5.0f), glm::vec3(0.0f, 1.0f, 0.0f));*/
-		light.model = glm::mat4(1.0);
-		light.position = glm::vec3(0.0f, 0.1f, 0.0f);
-		light.ambient = glm::vec3(0.2f);
-		light.diffuse = glm::vec3(0.5f);
-		light.specular = glm::vec3(1.0f);
-		vkMapMemory(device, DS_maze.uniformBuffersMemory[1][currentImage], 0, sizeof(Light), 0, &data);
-		memcpy(data, &light, sizeof(Light));
-		vkUnmapMemory(device, DS_maze.uniformBuffersMemory[1][currentImage]);
-
-		/*
-		Light lightCast{};
-		lightCast.position = glm::vec3(0.2f, 0.5f, -0.2f);
-		lightCast.ambient = glm::vec3(0.8f, 0.2f, 0.2f);
-		lightCast.diffuse = glm::vec3(0.8f, 0.2f, 0.2f);
-		lightCast.specular = glm::vec3(0.8f, 0.2f, 0.2f);
-		lightCast.model = glm::translate(glm::mat4(1.0f),
-			glm::vec3(lightCast.position)) * glm::rotate(glm::mat4(1.0f), time * glm::radians(70.0f + 5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		vkMapMemory(device, DS_treasure.uniformBuffersMemory[0][currentImage], 0, sizeof(Light), 0, &data);
-		memcpy(data, &lightCast, sizeof(Light));
-		vkUnmapMemory(device, DS_treasure.uniformBuffersMemory[0][currentImage]);
-		*/
+		//vkMapMemory(device, DS_maze.uniformBuffersMemory[0][currentImage], 0, sizeof(maze), 0, &data);
+		Material m{};
+		m.specular_color = maze.specular_color;
+		//vkMapMemory(device, maze.ds.uniformBuffersMemory[0][currentImage], 0, sizeof(m), 0, &data);
+		vkMapMemory(device, p_graphic.ds.uniformBuffersMemory[0][currentImage], 0, sizeof(m), 0, &data);
+		memcpy(data, &m, sizeof(m));
+		//vkUnmapMemory(device, maze.ds.uniformBuffersMemory[0][currentImage]);
+		vkUnmapMemory(device, p_graphic.ds.uniformBuffersMemory[0][currentImage]);
 
 	}
 
 	void localCleanup() {
 		stbi_image_free(map);
-	
-		DS_mvp.cleanup();
+		//DS_mvp.cleanup();
+		p_graphic.ds_global.cleanup();
 
-		DS_maze.cleanup();
-		T_maze.cleanup();
-		M_maze.cleanup();
+		//DS_maze.cleanup();
+		//T_maze.cleanup();
+		//M_maze.cleanup();
+		p_graphic.ds.cleanup();
+		maze.texture.cleanup();
+		maze.model.cleanup();
 
-		//DS_treasure.cleanup();
-
-		//T_treasure.cleanup();
-		//M_treasure.cleanup();
-
-		P_maze.cleanup();
-		//P_treasure.cleanup();
-		DSL_mvp.cleanup();
-		DSL_maze.cleanup();
-		//DSL_treasure.cleanup();
+		//P_maze.cleanup();
+		p_graphic.pipeline.cleanup();
+		//DSL_mvp.cleanup();
+		p_graphic.dsl_global.cleanup();
+		//DSL_maze.cleanup();
+		p_graphic.dsl.cleanup();
 	}
 
 };
